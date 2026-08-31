05d8546575549cd69f233134d705083845b7e2b9
senakiper1 <senanurkiper@gmail.com>
2026-08-31 22:23:55 +0300

    Remove unnecessary comments and whitespace

--- src/main/java/services/IPrescriptionService.java
+++ src/main/java/services/IPrescriptionService.java
@@ -6,8 +6,6 @@ import dto.PrescriptionDto;
 
 public interface IPrescriptionService {
     
-   
-    // Hastanın TC Kimlik No'suna göre reçetelerini getirmek için
-    List<PrescriptionDto> getPrescriptionsByPatientNationalId(String nationalId);
+       List<PrescriptionDto> getPrescriptionsByPatientNationalId(String nationalId);
     
 }
