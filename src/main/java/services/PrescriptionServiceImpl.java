2ed9be8fb1cd86498536e670abd9656fff118bcc
senakiper1 <senanurkiper@gmail.com>
2026-08-31 22:24:33 +0300

    Remove unnecessary whitespace and format code

--- src/main/java/services/PrescriptionServiceImpl.java
+++ src/main/java/services/PrescriptionServiceImpl.java
@@ -19,12 +19,10 @@ public class PrescriptionServiceImpl implements IPrescriptionService {
 
     @Override
     public List<PrescriptionDto> getPrescriptionsByPatientNationalId(String nationalId) {
-        // MedicalRecord -> Patient -> NationalId ilişkisi üzerinden veritabanından reçeteler çekilir
     	List<Prescription> prescriptions = prescriptionRepository.findByPatientNationalId(nationalId);        
         List<PrescriptionDto> dtoList = new ArrayList<>();
         for (Prescription prescription : prescriptions) {
             PrescriptionDto dto = new PrescriptionDto();
-            // id, medicineName, dosage, quantity alanlarını otomatik kopyalar
             BeanUtils.copyProperties(prescription, dto); 
             dtoList.add(dto);
         }
